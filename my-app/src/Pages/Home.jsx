61577f30599c1998becf9e65436f257ec796d9e9
Alibayejja <alibayejja604@gmail.com>
2025-11-27 09:24:15 +0000

    test

---
+++ my-app/src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+import "./Home.css";
+<section className="hero">
+    <div className="hero-content">
+        <h1>Voyage Culinaire à Travers le Monde</h1>
+        <p>Découvrez des recettes délicieuses venues du monde entier</p>
+        <button className="hero-btn">Commander maintenant</button>
+    </div>
+</section>
