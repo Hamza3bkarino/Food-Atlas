25a690c1546f2a858112f88c7f876e1e640ba6fc
BS4002 <bakkousalma@gmail.com>
2025-11-28 15:36:31 +0100

    Recette Détails

--- my-app/src/Components/Recette_Card.jsx
+++ my-app/src/Components/Recette_Card.jsx
@@ -1,40 +1,40 @@
 import './Recette_Card.css'
 import { GrFormView } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
-export default function RecetteCard({recettes}){
-    return(
-        <>
-          <div className="card">
-            <img id='Card_Img' src={recettes.image} alt={recettes.titre} srcset="" width={'270px'}/>
-
+export default function RecetteCard({recettes}) {
+    return (
+        <div className="card">
+            <img id='Card_Img' src={recettes.image} alt={recettes.titre} width={'270px'}/>
 
             <div className="card-body">
-              <div className="card-header">
-                <h3>{recettes.titre}</h3>
-                <span className="heart">❤</span>
-              </div>
+                <div className="card-header">
+                    <h3>{recettes.titre}</h3>
+                    <span className="heart">❤</span>
+                </div>
 
-              <div className="card-rating">
-                <span className="star filled">★</span>
-                <span className="star filled">★</span>
-                <span className="star filled">★</span>
-                <span className="star filled">★</span>
-                <span className="star filled">★</span>
-                <span className="rating-count">(5)</span>
-              </div>
+                <div className="card-rating">
+                    <span className="star filled">★</span>
+                    <span className="star filled">★</span>
+                    <span className="star filled">★</span>
+                    <span className="star filled">★</span>
+                    <span className="star filled">★</span>
+                    <span className="rating-count">(5)</span>
+                </div>
 
-              <p className="card-desc">
-                {recettes.description}
-              </p>
+                <p className="card-desc">
+                    {recettes.description}
+                </p>
 
-              <div className="card-footer">
-                <span className="price">{recettes.categorie}</span>
-                <button className="basket">
-                  <GrFormView/>
-                </button>
-              </div>
+                <div className="card-footer">
+                    <span className="price">{recettes.categorie}</span>
+
+                    {/* 🔥 Bouton qui mène vers la page détail */}
+                    <Link to={`/recette/${recettes.id}`} className="basket">
+                      <GrFormView />
+                    </Link>
+                </div>
             </div>
-            </div>
-           </>
+        </div>
     )
 }
