94887bafde152f7578060b198d9325e6cb287b0c
Hamza Abkarino <errajihamza80@gmail.com>
2025-12-02 09:27:45 +0100

    Fix DeletePopup

--- my-app/src/Components/DeletePopUp/DeletePopUp.jsx
+++ my-app/src/Components/DeletePopUp/DeletePopUp.jsx
@@ -1,17 +1,25 @@
-import './DeletePopup.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./DeletePopup.css";
 
-export default function DeletePopup({ onConfirm, onCancel ,loading }) {
-  return (
-    <div className="popup-overlay">
-      <div className="popup-box">
-        <h3>Êtes-vous sûr de vouloir supprimer ? </h3>
-        <p className="popup-text">Cette action est irréversible.</p>
+export default function DeletePopup({ onConfirm, onCancel, loading }) {
+  if (typeof document === "undefined") return null;
+
+  return ReactDOM.createPortal(
+    <div className="popup-overlay" onClick={onCancel}>
+      <div className="popup-box" onClick={(e) => e.stopPropagation()}>
+        <h3>Confirmez la suppression</h3>
+        <p>Êtes-vous sûr de vouloir supprimer cette recette ?</p>
 
         <div className="popup-buttons">
-          <button className="btn cancel" onClick={onCancel}>Annuler</button>
-          <button className="btn delete" onClick={onConfirm}>{loading ? <div className="mini-loader"></div> : 'Supprimer'} </button>
+          <button className="cancel-btn" onClick={onCancel}>Annuler</button>
+
+          <button className="delete-btn" onClick={onConfirm}>
+            {loading ? <div className="mini-loader"></div> : "Supprimer"}
+          </button>
         </div>
       </div>
-    </div>
+    </div>,
+    document.body
   );
 }
